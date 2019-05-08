380fc521e2a26b6b3e39e6516d90c434f70a8f03
iamprovidence <orleon.nilkford@gmail.com>
2019-05-08 14:40:52 +0300

    PercentagePaid close#68

---
+++ Apartments_io/Apartments_io/Areas/Manager/Models/PaymentStatusText.cs
@@ -0,0 +1,18 @@
+﻿namespace Apartments_io.Areas.Manager.Models
+{
+    public static class PaymentStatusText
+    {
+        public static string GetText(this DataAccess.Enums.PaymentStatus paymentStatus)
+        {
+            switch (paymentStatus)
+            {
+                case DataAccess.Enums.PaymentStatus.WaitingForPayment: return "Waiting for payment";
+                case DataAccess.Enums.PaymentStatus.Paid: return "Paid";
+                case DataAccess.Enums.PaymentStatus.Overdue: return "Overdue";
+                case DataAccess.Enums.PaymentStatus.PaidWithDelay: return "Paid with delay";
+
+                default: throw new System.InvalidOperationException("Wrong enum value");
+            }
+        }
+    }
+}
