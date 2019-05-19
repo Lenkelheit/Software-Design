83312e2358aa94c975da007d74cd2eb80e5a9e55
Nazar <tymtsiv2710@gmail.com>
2019-05-19 13:05:33 +0300

    Fix Administrator controller test

--- Apartments_io/Apartments_io.Tests/AreasTest/AdministratorTest/ControllersTest/UsersControllerTest.cs
+++ Apartments_io/Apartments_io.Tests/AreasTest/AdministratorTest/ControllersTest/UsersControllerTest.cs
@@ -1,29 +1,18 @@
 ﻿using Xunit;
 
-using Core.Extensions;
+using Moq;
 
 using DataAccess.Entities;
 using DataAccess.Repositories;
 using DataAccess.Interfaces;
 
-using Apartments_io.Controllers;
-using Apartments_io.ViewModels.Home;
-using Apartments_io.Models;
-
-using Moq;
-
-using System;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Apartments_io.Areas.Administrator.ViewModels.Users;
 using Apartments_io.Areas.Administrator.Controllers;
-using System.Collections;
+
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
 {
@@ -38,15 +27,11 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
             {
                 new User { FirstName = "John", Role = DataAccess.Enums.Role.Manager }, new User { FirstName = "Alan", Role = DataAccess.Enums.Role.Resident }
             };
-            IEnumerable<User> managers = users.Where(u => u.Role == DataAccess.Enums.Role.Manager);
 
             Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
             mockUserRepository
                 .Setup(ur => ur.Get(null, null, string.Empty, page, itemPerPageSize))
                 .Returns(users);
-            mockUserRepository
-                .Setup(ur => ur.GetUserByRole(DataAccess.Enums.Role.Manager))
-                .Returns(managers);
             mockUserRepository
                 .Setup(ur => ur.Count())
                 .Returns(users.Count());
@@ -64,11 +49,52 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
             // Assert
             Assert.NotNull(result);
             ViewResult viewResult = Assert.IsType<ViewResult>(result);
-            Assert.Equal(nameof(Areas.Administrator), viewResult.ViewData["Title"]);
             Assert.NotNull(viewResult.Model);
             IndexViewModel indexViewModel = Assert.IsType<IndexViewModel>(viewResult.Model);
             Assert.Same(users, indexViewModel.Users);
-            Assert.Same(managers, indexViewModel.Managers);
+        }
+
+        [Fact]
+        public void Managers_ViewResult()
+        {
+            // Arrange
+            IEnumerable<User> residents = new List<User>
+            {
+                new User { FirstName = "Alan", Role = DataAccess.Enums.Role.Resident }, new User { FirstName = "Eduard", Role = DataAccess.Enums.Role.Resident }
+            };
+            IEnumerable<User> managers = new List<User>
+            {
+                new User { FirstName = "John", Role = DataAccess.Enums.Role.Manager }, new User { FirstName = "Ann", Role = DataAccess.Enums.Role.Manager }
+            };
+
+            Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
+            mockUserRepository
+                .Setup(ur => ur.Get(u => u.Role == DataAccess.Enums.Role.Resident, null, string.Empty, It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(residents);
+            mockUserRepository
+                .Setup(ur => ur.GetUserByRole(DataAccess.Enums.Role.Manager))
+                .Returns(managers);
+            mockUserRepository
+                .Setup(ur => ur.Count())
+                .Returns(residents.Count());
+
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork
+                .Setup(u => u.GetRepository<User, UserRepository>())
+                .Returns(mockUserRepository.Object);
+
+            UsersController controller = new UsersController(mockUnitOfWork.Object);
+
+            // Act
+            IActionResult result = controller.Managers();
+
+            // Assert
+            Assert.NotNull(result);
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult.Model);
+            ManagersViewModel managersViewModel = Assert.IsType<ManagersViewModel>(viewResult.Model);
+            Assert.Same(residents, managersViewModel.Users);
+            Assert.Same(managers, managersViewModel.Managers);
         }
 
         [Fact]
@@ -76,7 +102,6 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
         {
             // Arrange
             User user = new User { Email = "first@gmail.com" };
-            int managerId = 2;
 
             Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
             mockUserRepository
@@ -91,7 +116,7 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
             UsersController controller = new UsersController(mockUnitOfWork.Object);
 
             // Act
-            IActionResult result = await controller.Create(user, managerId);
+            IActionResult result = await controller.Create(user);
 
             // Assert
             Assert.NotNull(result);
@@ -104,7 +129,6 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
         {
             // Arrange
             User user = new User { Email = "first@gmail.com" };
-            int managerId = 2;
 
             Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
             mockUserRepository
@@ -119,7 +143,7 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
             UsersController controller = new UsersController(mockUnitOfWork.Object);
 
             // Act
-            IActionResult result = await controller.Create(user, managerId);
+            IActionResult result = await controller.Create(user);
 
             // Assert
             Assert.NotNull(result);
@@ -131,7 +155,6 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
         {
             // Arrange
             User user = new User { Email = "first@gmail.com" };
-            int managerId = 2;
 
             Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
 
@@ -143,7 +166,96 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
             UsersController controller = new UsersController(mockUnitOfWork.Object);
 
             // Act
-            IActionResult result = await controller.Update(user, managerId);
+            IActionResult result = await controller.Update(user);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async void UpdateManagerNoUser_BadRequest()
+        {
+            // Arrange
+            User resident = new User { Id = 1, Email = "first@gmail.com", Role = DataAccess.Enums.Role.Resident };
+            User manager = new User { Id = 2, Email = "second@gmail.com", Role = DataAccess.Enums.Role.Manager };
+
+            Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
+            mockUserRepository
+                .Setup(ur => ur.GetAsync(resident.Id, string.Empty))
+                .Returns(Task.FromResult(null as User));
+
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork
+                .Setup(u => u.GetRepository<User, UserRepository>())
+                .Returns(mockUserRepository.Object);
+
+            UsersController controller = new UsersController(mockUnitOfWork.Object);
+
+            // Act
+            IActionResult result = await controller.UpdateManager(resident.Id, manager.Id);
+
+            // Assert
+            Assert.NotNull(result);
+            BadRequestObjectResult badRequestObjectResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("There is no user with such id", badRequestObjectResult.Value.ToString());
+        }
+
+        [Fact]
+        public async void UpdateManagerNoManager_BadRequest()
+        {
+            // Arrange
+            User resident = new User { Id = 1, Email = "first@gmail.com", Role = DataAccess.Enums.Role.Resident };
+            User manager = new User { Id = 2, Email = "second@gmail.com", Role = DataAccess.Enums.Role.Manager };
+
+            Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
+            mockUserRepository
+                .Setup(ur => ur.GetAsync(resident.Id, string.Empty))
+                .Returns(Task.FromResult(resident));
+            mockUserRepository
+                .Setup(ur => ur.GetAsync(manager.Id, string.Empty))
+                .Returns(Task.FromResult(null as User));
+
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork
+                .Setup(u => u.GetRepository<User, UserRepository>())
+                .Returns(mockUserRepository.Object);
+
+            UsersController controller = new UsersController(mockUnitOfWork.Object);
+
+            // Act
+            IActionResult result = await controller.UpdateManager(resident.Id, manager.Id);
+
+            // Assert
+            Assert.NotNull(result);
+            BadRequestObjectResult badRequestObjectResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("There is no manager with such id", badRequestObjectResult.Value.ToString());
+        }
+
+        [Fact]
+        public async void UpdateManager_OkResult()
+        {
+            // Arrange
+            User resident = new User { Id = 1, Email = "first@gmail.com", Role = DataAccess.Enums.Role.Resident };
+            User manager = new User { Id = 2, Email = "second@gmail.com", Role = DataAccess.Enums.Role.Manager };
+
+            Mock<UserRepository> mockUserRepository = new Mock<UserRepository>();
+            mockUserRepository
+                .Setup(ur => ur.GetAsync(resident.Id, string.Empty))
+                .Returns(Task.FromResult(resident));
+            mockUserRepository
+                .Setup(ur => ur.GetAsync(manager.Id, string.Empty))
+                .Returns(Task.FromResult(manager));
+
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork
+                .Setup(u => u.GetRepository<User, UserRepository>())
+                .Returns(mockUserRepository.Object);
+
+            UsersController controller = new UsersController(mockUnitOfWork.Object);
+
+            // Act
+            IActionResult result = await controller.UpdateManager(resident.Id, manager.Id);
 
             // Assert
             Assert.NotNull(result);
@@ -244,6 +356,5 @@ namespace Apartments_io.Tests.AreasTest.AdministratorTest.ControllersTest
             Assert.NotNull(result);
             Assert.IsType<OkResult>(result);
         }
-
     }
 }
