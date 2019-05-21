8b3e7a89ab4fa341f3274d695c47cfadc26df381
iamprovidence <orleon.nilkford@gmail.com>
2019-05-21 15:43:18 +0300

    Fix scripts close#101, close#71

---
+++ Apartments_io/Apartments_io/wwwroot/js/areas/resident/apartments/list.js
@@ -0,0 +1,13 @@
+﻿// FILTER SUBMIT
+$("#filter-form").submit(function (event)
+{
+    var minPrice = $("#filter-min-price").val();
+    var maxPrice = $("#filter-max-price").val();
+
+    // validate
+    if (minPrice > maxPrice)
+    {
+        event.preventDefault();
+        ohSnap("Min price can not be higher than max price", { color: 'red' });
+    }
+});
